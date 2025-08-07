0ba370eecf9c154983e05039f8f0f81c1b993426
anjal <anjaliraj3636@gmail.com>
2025-08-07 17:53:31 +0530

    ReverseString

--- JavaCoding/src/com/dsa/ReverString.java
+++ JavaCoding/src/com/dsa/ReverString.java
@@ -7,7 +7,7 @@ public class ReverString {
         String str =sc.nextLine();
         String rev= "";
         for(int i=str.length()-1;i>=0;i--) {
-            rev=rev+str.charAt(i) ;
+            rev=rev+(str.charAt(i) );
         }
 
         System.out.println(rev);
