5747493a340c213c3181ca083e10e60eefceaf5e
anjal <anjaliraj3636@gmail.com>
2025-08-07 18:17:08 +0530

    SwapTwoNumber

---
+++ JavaCoding/src/com/dsa/SwapTwoNumber.java
@@ -0,0 +1,26 @@
+package com.dsa;
+
+import java.util.Scanner;
+
+public class SwapTwoNumber {
+    public  static void swapNumber() {
+
+        System.out.println("inter number a,b:");
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a+" "+b);
+    }
+
+    public static void main(String[] args) {
+
+        swapNumber();
+
+    }
+
+}
+
+
