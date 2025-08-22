06b33575c6dc4fbcb72653af3caf9a606516bb2e
anjal <anjaliraj3636@gmail.com>
2025-08-22 19:40:45 +0530

    SumOfNaturalNumber

---
+++ JavaCoding/src/com/dsa/LargestEelmentsInAnArray.java
@@ -0,0 +1,16 @@
+package com.dsa;
+
+public class LargestEelmentsInAnArray {
+    public static void main(String[] args) {
+        int arr[]= {1,3,5,8,4,7};
+        int largest=arr[0];
+        for(int n:arr){
+            if(n>largest) {
+                largest=n;
+            }
+
+        }
+        System.out.println(largest);
+    }
+
+}
