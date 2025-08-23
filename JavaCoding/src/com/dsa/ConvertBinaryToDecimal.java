7e7ec4a07d195ed96de5f7471040c24cd128ff39
anjal <anjaliraj3636@gmail.com>
2025-08-23 21:29:00 +0530

    ConvertBinaryToDecimal

---
+++ JavaCoding/src/com/dsa/ConvertBinaryToDecimal.java
@@ -0,0 +1,9 @@
+package com.dsa;
+
+public class ConvertBinaryToDecimal {
+    public static void main(String[] args) {
+        String num ="1010";
+        int decimal=Integer.parseInt(num,2);
+        System.out.println(decimal);
+    }
+}
