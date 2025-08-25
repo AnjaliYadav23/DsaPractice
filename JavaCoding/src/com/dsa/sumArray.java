cb54296cc6fb813ce6ab60079399ecafd123b136
anjal <anjaliraj3636@gmail.com>
2025-08-25 21:54:01 +0530

    sumArray

---
+++ JavaCoding/src/com/dsa/sumArray.java
@@ -0,0 +1,12 @@
+package com.dsa;
+
+public class sumArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.print(sum);
+    }
+}
