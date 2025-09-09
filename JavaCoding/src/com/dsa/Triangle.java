676f0d9c010921776a8b3626b09648d3a6034247
anjal <anjaliraj3636@gmail.com>
2025-09-09 21:44:58 +0530

    Triangle

---
+++ JavaCoding/src/com/dsa/Triangle.java
@@ -0,0 +1,15 @@
+package com.dsa;
+
+public class Triangle {
+    public static void main(String[] args) {
+
+        int a = 5;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+        }
+    }
