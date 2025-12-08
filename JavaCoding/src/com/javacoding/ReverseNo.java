1441d76c6e9264e19f03b6aa046cdf8f31200e0c
anjal <anjaliraj3636@gmail.com>
2025-12-08 17:02:33 +0530

    ReverseNumber

--- JavaCoding/src/com/javacoding/ReverseNo.java
+++ JavaCoding/src/com/javacoding/ReverseNo.java
@@ -5,9 +5,9 @@ public class ReverseNo {
     public static void main(String[] args) {
         int num = 234;
         int rev = 0;
-        int rem = 0;
+
         while (num > 0) {
-             rem = num % 10;
+          int rem = num % 10;
             rev = rem + (rev * 10);
             num = num / 10;
         }
