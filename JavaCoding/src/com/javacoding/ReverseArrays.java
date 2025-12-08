ef382f43701ede602b881a98d535a1835c59e143
anjal <anjaliraj3636@gmail.com>
2025-12-08 15:18:16 +0530

    ReverseArrays

---
+++ JavaCoding/src/com/javacoding/ReverseArrays.java
@@ -0,0 +1,10 @@
+package com.javacoding;
+
+public class ReverseArrays {
+    public static void main(String[] args){
+        int arr[]={2,4,8,9};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
