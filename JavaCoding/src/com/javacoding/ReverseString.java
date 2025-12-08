d78cd8b531e95c47b74e91dde0ba5be6be6a9302
anjal <anjaliraj3636@gmail.com>
2025-12-08 14:00:08 +0530

    ReverseString

--- JavaCoding/src/com/javacoding/ReverseString.java
+++ JavaCoding/src/com/javacoding/ReverseString.java
@@ -1,7 +1,7 @@
 package com.javacoding;
 
 public class ReverseString {
-    public static void main (String args[]){
+    public static void main (String[] args){
         String str= "Madam";
         str=str.toLowerCase();
         String Reversed=" ";
