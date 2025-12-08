be9ae232278950f285ca483b0b1e70934d6c36c1
anjal <anjaliraj3636@gmail.com>
2025-12-08 16:26:34 +0530

    Largest Number In Array

--- JavaCoding/src/com/javacoding/LargestNoInArray.java
+++ JavaCoding/src/com/javacoding/LargestNoInArray.java
@@ -3,7 +3,7 @@ package com.javacoding;
 public class LargestNoInArray {
     public static void main(String[] args) {
 
-        int arr[] = {3, 81, 8, 5};
+        int[] arr = {3, 81, 8, 5};
         int largest=arr[0];
         for (int i = 0; i <= arr.length - 1; i++) {
             if(arr[i]>largest){
