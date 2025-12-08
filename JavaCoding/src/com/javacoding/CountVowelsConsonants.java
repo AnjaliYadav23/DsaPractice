b931818018993cc7c9fbcb02dcf50ee99f763fad
anjal <anjaliraj3636@gmail.com>
2025-12-08 14:57:52 +0530

    CountVowelsConsonants

---
+++ JavaCoding/src/com/javacoding/CountVowelsConsonants.java
@@ -0,0 +1,24 @@
+package com.javacoding;
+
+public class CountVowelsConsonants {
+    public static void main(String[] args){
+        String str="Abcde";
+        str=str.toLowerCase();
+        int vowels=0;
+        int consonants=0;
+        for(int i=0;i<=str.length()-1;i++){
+            char ch =str.charAt(i);
+            if(ch>='a' && ch<='z'){
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    vowels++;
+                }
+                else{
+                    consonants++;
+                }
+            }
+
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
