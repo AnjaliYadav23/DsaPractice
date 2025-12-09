99657800db825440da35c40c334a39a86234a189
anjal <anjaliraj3636@gmail.com>
2025-12-09 20:25:28 +0530

    SwapNumbers

---
+++ JavaCoding/src/com/javacoding/SwapNumbers.java
@@ -0,0 +1,15 @@
+package com.javacoding;
+
+public class SwapNumbers {
+    public static void main (String[] args) {
+int a=5;
+int b=7;
+int temp=a;
+ a=b;
+ b=temp;
+        System.out.println("After Swap:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    }
