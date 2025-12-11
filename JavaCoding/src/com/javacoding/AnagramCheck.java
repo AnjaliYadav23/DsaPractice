36b91b8c9c715bf5dd9ae6a815d31a67a5076376
anjal <anjaliraj3636@gmail.com>
2025-12-11 21:08:35 +0530

    AnagramCheck

---
+++ JavaCoding/src/com/javacoding/AnagramCheck.java
@@ -0,0 +1,24 @@
+package com.javacoding;
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+
+        String s1 = "listen";
+        String s2 = "silent";
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+       char[] arr=s1.toCharArray();
+       char[] arr1=s2.toCharArray();
+        Arrays.sort(arr);
+        Arrays.sort(arr1);
+if(Arrays.equals(arr,arr1)){
+    System.out.println("Anagram");
+} else {
+    System.out.println("Not Anagram");
+}
+        }
+    }
+
+
