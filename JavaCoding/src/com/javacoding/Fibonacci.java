468d42c4bc4bcdf904d2116c54903a41d64e7ea7
anjal <anjaliraj3636@gmail.com>
2025-12-11 20:46:19 +0530

    Fibonacci

---
+++ JavaCoding/src/com/javacoding/Fibonacci.java
@@ -0,0 +1,16 @@
+package com.javacoding;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n=5;
+    int a=0;
+    int b=1;
+        System.out.print(a + " " + b +" ");
+    for(int i=1;i<=n;i++){
+        int c=a+b;
+        System.out.print(c+ " " );
+        a=b;
+        b=c;
+    }
+    }
+    }
