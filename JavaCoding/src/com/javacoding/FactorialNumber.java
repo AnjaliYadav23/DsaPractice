c5b267266c3175172422a882e0a82dab4da6a271
anjal <anjaliraj3636@gmail.com>
2025-12-11 20:35:11 +0530

    FactorialNumber

---
+++ JavaCoding/src/com/javacoding/FactorialNumber.java
@@ -0,0 +1,13 @@
+package com.javacoding;
+
+public class FactorialNumber {
+    public static void main(String[] args) {
+    int num=4;
+    int fact=1;
+    for(int i=1;i<=num;i++){
+        fact*=i;
+    }
+        System.out.println("Factorial of " + num + " is: " + fact);
+
+    }
+    }
