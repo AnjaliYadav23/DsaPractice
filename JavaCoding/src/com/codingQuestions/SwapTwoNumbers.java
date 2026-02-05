452c6b6da965d2349b5e8d7f3d8358ee082ce351
anjal <anjaliraj3636@gmail.com>
2026-02-05 16:34:30 +0530

    SwapNumbersWithoutUsingThirdVar

---
+++ JavaCoding/src/com/codingQuestions/SwapTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.codingQuestions;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+     int a=6 ,b=5;
+      a=a+b;
+      b=a-b;
+      a=a-b;
+      System.out.print(a+" "+b);
+    }
+}
