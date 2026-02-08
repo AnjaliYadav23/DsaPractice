ab15a0a5370d40fd3094cae299c867e839996b77
anjal <anjaliraj3636@gmail.com>
2026-02-08 21:11:17 +0530

    Factorial

---
+++ JavaCoding/src/com/codingQuestions/Factorial.java
@@ -0,0 +1,12 @@
+package com.codingQuestions;
+
+public class Factorial {
+    public static  void main(String[] args) {
+        int num=6;
+        int fact=1;
+       for(int i=num;i>0;i--){
+          fact*=i;
+        }
+        System.out.println(fact);
+    }
+}
