afba953ad6b2c8e7d85bb8a03d8743e921089128
anjal <anjaliraj3636@gmail.com>
2026-02-17 18:00:23 +0530

    PrimeNumber

---
+++ JavaCoding/src/com/codingQuestions/PrimeNumber.java
@@ -0,0 +1,26 @@
+package com.codingQuestions;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num=5;
+        boolean isPrime=true;
+        if(num<=1){
+            isPrime=false;
+        }
+        else{
+            for(int i=2;i<num;i++){
+                if(num%i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+        }
+        if(isPrime){
+            System.out.println(num + " is Prime");
+        } else {
+            System.out.println(num + " is Not Prime");
+        }
+        }
+
+
+    }
