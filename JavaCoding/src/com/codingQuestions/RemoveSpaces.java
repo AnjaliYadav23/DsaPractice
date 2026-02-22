edf96593485a5187bb71893444e2dc7f609dea8b
anjal <anjaliraj3636@gmail.com>
2026-02-22 23:23:57 +0530

    RemoveSpaces

---
+++ JavaCoding/src/com/codingQuestions/RemoveSpaces.java
@@ -0,0 +1,9 @@
+package com.codingQuestions;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = " Hi I am Anjali Kumari";
+               str=str.replace(" ","");
+        System.out.println(str);
+    }
+}
