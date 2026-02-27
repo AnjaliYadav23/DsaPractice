c7a9162c0bef1ec3aaa926aa27260954dc632aff
anjal <anjaliraj3636@gmail.com>
2026-02-27 22:01:42 +0530

    StringLength

---
+++ JavaCoding/src/com/codingQuestions/StringLength.java
@@ -0,0 +1,10 @@
+package com.codingQuestions;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Anjali";
+        int stringCount=str.length();
+                System.out.println(stringCount);
+            }
+
+}
