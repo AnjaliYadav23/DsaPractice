a7d30e3b9b5d6f7034e754bdb873be327b07e701
anjal <anjaliraj3636@gmail.com>
2026-03-05 21:05:35 +0530

    StringIsEmpty

---
+++ JavaCoding/src/com/codingQuestions/StringIsEmpty.java
@@ -0,0 +1,10 @@
+package com.codingQuestions;
+
+public class StringIsEmpty {
+        public static void main(String[] args) {
+            String string = "A";
+            System.out.println(string.isEmpty());
+        }
+    }
+
+
