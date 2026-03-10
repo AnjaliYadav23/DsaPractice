10a4fe918fcbdb042ae7869f29d4a071aa14a280
anjal <anjaliraj3636@gmail.com>
2026-03-10 20:16:35 +0530

    Table

---
+++ JavaCoding/src/com/codingQuestions/Table.java
@@ -0,0 +1,10 @@
+package com.lnctu.JavaCoding.src.com.codingQuestions;
+
+public class Table {
+    public static void main(String[] args) {
+        int num=2;
+        for(int i=1;i<=10;i++) {
+            System.out.println(num+"*"+i+"="+(i*num));
+        }
+    }
+    }
