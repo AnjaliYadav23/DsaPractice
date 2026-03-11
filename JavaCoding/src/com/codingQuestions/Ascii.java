4971a7f1e6c7989ebb93932c0ab142f2c22feb8e
anjal <anjaliraj3636@gmail.com>
2026-03-11 18:36:05 +0530

    Ascii

---
+++ JavaCoding/src/com/codingQuestions/Ascii.java
@@ -0,0 +1,10 @@
+package com.lnctu.JavaCoding.src.com.codingQuestions;
+
+public class Ascii {
+    public static void main(String[] args) {
+        char Character ='A';
+        int ascii=(int)Character;
+        System.out.println(ascii);
+    }
+    }
+
